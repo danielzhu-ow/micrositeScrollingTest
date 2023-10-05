c09eaab40dee5addf30da709b603c577b0f453a4
Daniel Zhu <136386890+danielZhu-lippincott@users.noreply.github.com>
2023-10-05 16:43:28 -0400

    Scrolling GIFS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
 import logo from './logo.svg';
+import spriteSheet from './gifSpriteSheet.png'
+import fallingSheet from './fallingSheet.png'
+import { useState } from 'react'
+import { scroll } from "framer-motion/dom"
 import './App.css';
 
+import ScrollingImage from './ScrollingImage';
+
 function App() {
+  const [scrollProgress, setScrollProgress] = useState(0)
+  scroll((progress) => {
+    setScrollProgress(progress)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ position: "relative" }}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <ScrollingImage pos={[0, 0]} spriteSrc={spriteSheet} imgWidth={480} imgHeight={360} frames={27} scrollProgress={scrollProgress} scrollStart={0} scrollDuration={2} displayWidth={1000} />
+          <ScrollingImage pos={[1000, 0]} spriteSrc={fallingSheet} imgWidth={390} imgHeight={480} frames={27} scrollProgress={scrollProgress} scrollStart={0} scrollDuration={1} />
+          <Article />
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function Article() {
+  return (
+    <article>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
+        rhoncus quam.
+      </p>
+      <p>
+        Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
+        imperdiet sagittis justo. In viverra fermentum ex ac vestibulum. Aliquam
+        eleifend nunc a luctus porta. Mauris laoreet augue ut felis blandit, at
+        iaculis odio ultrices. Nulla facilisi. Vestibulum cursus ipsum tellus,
+        eu tincidunt neque tincidunt a.
+      </p>
+      <h2>Sub-header</h2>
+      <p>
+        In eget sodales arcu, consectetur efficitur metus. Duis efficitur
+        tincidunt odio, sit amet laoreet massa fringilla eu.
+      </p>
+      <p>
+        Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna. Mauris
+        id mauris vel arcu commodo venenatis. Aliquam eu risus arcu. Proin sit
+        amet lacus mollis, semper massa ut, rutrum mi.
+      </p>
+      <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+      <p>
+        Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra leo
+        vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+      </p>
+      <h2>Sub-header</h2>
+      <p>
+        Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+        aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat sem,
+        sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula metus
+        ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac enim vel
+        felis pharetra laoreet. Interdum et malesuada fames ac ante ipsum primis
+        in faucibus. Pellentesque hendrerit ac augue quis pretium.
+      </p>
+      <p>
+        Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+        elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+        ultricies, mollis mi in, euismod dolor.
+      </p>
+      <p>
+        Quisque convallis ligula non magna efficitur tincidunt.
+      </p>
+      <p>
+        Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna. Mauris
+        id mauris vel arcu commodo venenatis. Aliquam eu risus arcu. Proin sit
+        amet lacus mollis, semper massa ut, rutrum mi.
+      </p>
+      <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+      <p>
+        Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra leo
+        vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+      </p>
+      <h2>Sub-header</h2>
+      <p>
+        Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+        aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat sem,
+        sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula metus
+        ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac enim vel
+        felis pharetra laoreet. Interdum et malesuada fames ac ante ipsum primis
+        in faucibus. Pellentesque hendrerit ac augue quis pretium.
+      </p>
+      <p>
+        Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+        elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+        ultricies, mollis mi in, euismod dolor.
+      </p>
+      <p>
+        Quisque convallis ligula non magna efficitur tincidunt.
+      </p>
+    </article>
+  )
+}
