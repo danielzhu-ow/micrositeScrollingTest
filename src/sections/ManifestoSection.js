5670236269ba0b1165f7831d7509cf02891f37ea
Daniel Zhu <136386890+danielZhu-lippincott@users.noreply.github.com>
2023-10-20 10:12:47 -0400

    Timing Calculation

--- src/sections/ManifestoSection.js
+++ src/sections/ManifestoSection.js
@@ -16,14 +16,38 @@ export { ManifestoSection }
 
 function ManifestoSection({ images }) {
 
+    //Heights
+    const sectionHeights = [200, 500, 500]
+    const sum = sectionHeights.reduce((partialSum, a) => partialSum + a, 0)
+
+    //Timings
+    const sectionTimings = [[[0, 1], [0, 0.6], [0, 0.6]], [[]], [[]]]
+    let adjustedTimings = []
+
+    for (let i = 0; i < sectionHeights.length; i++) {
+        let start = sectionHeights.slice(0, i).reduce((partialSum, a) => partialSum + a, 0) / sum
+        let localSum = sectionHeights[i] / sum
+        let adjusted = sectionTimings[i]
+        for (let j = 0; j < adjusted.length; j++) {
+            adjusted[j] = adjusted[j].map(c => c * localSum + start)
+        }
+        adjustedTimings.push(adjusted)
+    }
+
+    console.log(adjustedTimings)
+
     return (
         <div style={{ position: "relative", height: "100%", width: "100%" }}>
-            <Background background={images.manifesto_gradient} height={200} />
-            <FadingHeader type={"manifesto"} scrollInfo={[0, 0.2]} />
-            <TransformingContent child={<ImgBox url={images.manifesto_01} displayDimensions={[80, 80]} rotate={0} />} positions={[[0, -50], [-10, -60]]} scrollInfo={[0, 0.5]} alignment={['left', 'top']} />
-            <TransformingContent child={<ImgBox url={images.manifesto_02} displayDimensions={[100, 100]} rotate={0} />} positions={[[0, -60], [-5, -65]]} scrollInfo={[0, 0.5]} alignment={['right', 'bottom']} />
+            {/* HEADER */}
+            <Background background={images.manifesto_gradient} height={sectionHeights[0]} />
+            <FadingHeader type={"manifesto"} scrollInfo={adjustedTimings[0][0]} />
+            <TransformingContent child={<ImgBox url={images.manifesto_01} displayDimensions={[80, 80]} rotate={0} />} positions={[[0, -50], [-10, -60]]} scrollInfo={adjustedTimings[0][1]} alignment={['left', 'top']} />
+            <TransformingContent child={<ImgBox url={images.manifesto_02} displayDimensions={[100, 100]} rotate={0} />} positions={[[0, -60], [-5, -65]]} scrollInfo={adjustedTimings[0][2]} alignment={['right', 'bottom']} />
 
-            <Background background={"#202020"} height={100} />
+            {/* SECTION 1 */}
+            <Background background={"#202020"} height={sectionHeights[1]} />
+
+            <Background background={images.manifesto_gradient} height={sectionHeights[2]} />
         </div>
     )
 }
