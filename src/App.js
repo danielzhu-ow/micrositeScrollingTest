fbfcba96a6702df2a7ae4f01bfd2603db60511f9
Daniel Zhu <136386890+danielZhu-lippincott@users.noreply.github.com>
2023-11-20 09:56:37 -0500

    Scroll Buttons

--- src/App.js
+++ src/App.js
@@ -16,21 +16,21 @@ function App() {
   const [article, setArticle] = useState('naming experiment')
 
   const MANIFESTO_IMAGES = useImageLoader();
-  
+
 
   return (
     <div className="App">
       <div className='App-header'>
-        
+
         {article === 'naming' && <NamingSection text={NAMING_STRINGS} />}
         {article === 'manifesto' && <ManifestoSection images={MANIFESTO_IMAGES} />}
-        {article === 'naming experiment' && <NamingExperimentSection/>}
+        {article === 'naming experiment' && <NamingExperimentSection />}
         {article === 'hotdog' && <HotDogSection />}
 
-        <div style={{position: 'fixed', top: 0, color: 'white'}}>
+        <div style={{ position: 'fixed', top: 0, color: 'white' }}>
           {/* <button onClick={() => {setArticle('manifesto'); console.log('manifesto')}}>Manifesto</button> */}
-          <button onClick={() => {setArticle('naming'); console.log('naming')}}>Naming</button>
-          <button onClick={() => {setArticle('naming experiment'); console.log('naming experiment')}}>Naming Experiment</button>
+          <button onClick={() => { setArticle('naming'); console.log('naming'); window.scrollTo(0, 0) }}>Naming</button>
+          <button onClick={() => { setArticle('naming experiment'); console.log('naming experiment'); window.scrollTo(0, 0) }}>Naming Experiment</button>
           {/* <button onClick={() => {setArticle('hotdog'); console.log('hotdog')}}>Hotdog</button> */}
         </div>
       </div>
